82ea42cfc2c381b5d785216de33e86dc5f32ecd9
Harsha Vardhan <charagon@usc.edu>
2023-02-13 12:07:47 -0800

    Added and restructured files in components

--- client/src/pages/allProperties.tsx
+++ client/src/pages/allProperties.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Add } from "@mui/icons-material";
 
-const allProperties = () => {
-  return (
-    <div>allProperties</div>
-  )
-}
+import { Box, Stack, Typography } from "@pankod/refine-mui";
+import { useList } from "@pankod/refine-core";
+import { useNavigate } from "@pankod/refine-react-router-v6";
 
-export default allProperties
+// import { PropertyCard, CustomButton } from "components";
+
+const AllProperties = () => {
+	const navigate = useNavigate();
+	return (
+		<Box>
+			<Stack
+				direction="row"
+				justifyContent="space-between"
+				alignItems="center"
+			>
+				<Typography fontSize={25} fontWeight={700} color="#11142D">
+					Properties
+				</Typography>
+			</Stack>
+		</Box>
+	);
+};
+
+export default AllProperties;
