ef798c263b39071723ae4658deae26c93e7741cb
Harsha Vardhan <charagon@usc.edu>
2023-02-17 13:09:36 -0800

    Added Piecharts

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, Stack } from "@pankod/refine-mui";
 
-const PieChart = () => {
-  return (
-    <div>PieChart</div>
-  )
-}
+import { PieChartProps } from "interfaces/home";
+import ReactApexChart from "react-apexcharts";
 
-export default PieChart
+// const PieChart = ({title="", value={}, series={}, colors={}}) => { This also Works
+
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
+	return (
+		<Box
+			id="chart"
+			flex={1}
+			display="flex"
+			bgcolor="#fcfcfc"
+			flexDirection="row"
+			justifyContent="space-between"
+			alignItems="center"
+			pl={3.5}
+			py={2}
+			gap={2}
+			borderRadius="15px"
+			minHeight="110px"
+			width="fit-content"
+		>
+			<Stack>
+				<Typography fontSize={14} color="#808191">
+					{title}
+				</Typography>
+				<Typography
+					fontSize={24}
+					color="#11142d"
+					fontWeight="700"
+					mt={1}
+				>
+					{value}
+				</Typography>
+				<ReactApexChart
+					options={{
+						chart: { type: "donut" },
+						colors,
+						legend: { show: false },
+						dataLabels: { enabled: false },
+					}}
+					series={series}
+					type="donut"
+					width="120px"
+				/>
+			</Stack>
+		</Box>
+	);
+};
+
+export default PieChart;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {useList } from '@pankod/refine-core'
+
+import { PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from '../components/components_index'
+
+import { Typography, Box, Stack } from '@pankod/refine-mui'
 
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+      
+
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart title="Properties for sale" value={684} series={[75,25]} colors={['#475be8','#e4e8ef']}/>
+        <PieChart title="Properties for Rent" value={498} series={[60,40]} colors={['#475be8','#e4e8ef']}/>
+        <PieChart title="Total Customers" value={5684} series={[54,46]} colors={['#475be8','#e4e8ef']}/>
+        <PieChart title="Properties for Cities" value={555} series={[79,21]} colors={['#475be8','#e4e8ef']}/>
+
+       
+      </Box>
+      </Box>
   )
 }
 
