9771d5548fe7b544faf5105dc5e614c5c15f7f89
Harsha Vardhan <charagon@usc.edu>
2023-02-18 12:45:33 -0800

    Added Create Property Functionality

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,9 +1,193 @@
-import React from 'react'
+import React from "react";
+import { FormProps } from "interfaces/common";
+import CustomButton from "./CustomButton";
+import {
+	Box,
+	Typography,
+	FormControl,
+	FormHelperText,
+	TextField,
+	TextareaAutosize,
+	Stack,
+	Select,
+	MenuItem,
+	Button,
+} from "@pankod/refine-mui";
 
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+const Form = ({
+	type,
+	register,
+	propertyImage,
+	formLoading,
+	handleSubmit,
+	handleImageChange,
+	onFinish,
+	onFinishHandler,
+}: FormProps) => {
+	return (
+		<Box>
+			<Typography fontSize={25} fontWeight={600} color="#11142d">
+				{type} a Property
+			</Typography>
+			<Box mt={2.5} borderRadius="15px" padding="20px">
+				<form
+					style={{
+						marginTop: "20px",
+						width: "100%",
+						display: "flex",
+						flexDirection: "column",
+						gap: "20px",
+					}}
+					onSubmit={handleSubmit(onFinishHandler)}
+				>
+					<FormControl>
+						<FormHelperText
+							sx={{
+								fontWeight: 500,
+								margin: "10px",
+								fontSize: 16,
+								color: "#11142d",
+							}}
+						>
+							Enter Property Name
+						</FormHelperText>
+						<TextField
+							fullWidth
+							required
+							id="outlined-basic"
+							color="info"
+							variant="outlined"
+							{...register("title", { required: true })}
+						/>
+					</FormControl>
+					<FormControl>
+						<FormHelperText
+							sx={{
+								fontWeight: 500,
+								margin: "10px",
+								fontSize: 16,
+								color: "#11142d",
+							}}
+						>
+							Enter Description
+						</FormHelperText>
+						<TextareaAutosize
+							minRows={5}
+							required
+							placeholder="Write Description"
+							color="info"
+							style={{
+								width: "100%",
+								background: "transparent",
+								fontSize: "16px",
+								borderColor: "rgba(0,0,0,0.23)",
+								borderRadius: 6,
+								padding: 10,
+								color: "#919191",
+							}}
+							{...register("description", { required: true })}
+						/>
+					</FormControl>
+					<Stack direction="row" gap={4}>
+						<FormControl sx={{ flex: 1 }}>
+							<FormHelperText
+								sx={{
+									fontWeight: 500,
+									margin: "10px",
+									fontSize: 16,
+									color: "#11142d",
+								}}
+							>
+								Select Property Type
+							</FormHelperText>
+							<Select
+								variant="outlined"
+								color="info"
+								displayEmpty
+								required
+								defaultValue="Apartment"
+								inputProps={{ "aria-label": "Without label" }}
+								{...register("propertyType", {
+									required: true,
+								})}
+							>
+								<MenuItem value="Apartment">Apartment</MenuItem>
+								<MenuItem value="Villa">Villa</MenuItem>
+								<MenuItem value="IndependentHouse">
+									Independent House
+								</MenuItem>
+								<MenuItem value="Studio">Studio</MenuItem>
+							</Select>
+						</FormControl>
+						<FormControl>
+							<FormHelperText
+								sx={{
+									fontWeight: 500,
+									margin: "10px",
+									fontSize: 16,
+									color: "#11142d",
+								}}
+							>
+								Enter Rent Price
+							</FormHelperText>
+							<TextField
+								fullWidth
+								required
+								id="outlined-basic"
+								color="info"
+								variant="outlined"
+								type="number"
+								{...register("price", { required: true })}
+							/>
+						</FormControl>
+					</Stack>
+					<FormControl>
+						<FormHelperText
+							sx={{
+								fontWeight: 500,
+								margin: "10px",
+								fontSize: 16,
+								color: "#11142d",
+							}}
+						>
+							Enter Location
+						</FormHelperText>
+						<TextField
+							fullWidth
+							required
+							id="outlined-basic"
+							color="info"
+							variant="outlined"
+							{...register("location", { required: true })}
+						/>
+					</FormControl>
+					<Stack
+						direction="column"
+						gap={1}
+						justifyContent="center"
+						mb={2}
+					>
+						<Stack direction="row" gap={2}>
+							<Typography
+								color="#11142d"
+								fontSize={16}
+								fontWeight={500}
+								my="10px"
+							>
+								Property Photo
+							</Typography>
+							<Button>
+								Upload
+								<input type="file" hidden accept="image/*"/>
+							</Button>
+						</Stack>
+            <Typography fontSize={14} color="" sx={{wordBreak: 'break-all'}}>{propertyImage?.name}</Typography>
+					</Stack>
+          <CustomButton type="submit" title={ formLoading? 'Submitting': 'Submit'} backgroundColor="#475be8" color="#fcfcfc"/>
+				</form>
+			</Box>
+		</Box>
+	);
+};
 
-export default Form
+export default Form;
--- client/src/pages/createProperty.tsx
+++ client/src/pages/createProperty.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "@pankod/refine-react-router-v6";
+import { FieldValues, useForm } from "@pankod/refine-react-hook-form";
+import { useGetIdentity } from "@pankod/refine-core";
+import Form from "components/common/Form";
+const CreateProperty = () => {
+	const navigate = useNavigate();
+	const [propertyImage, setPropertyImage] = useState({ name: "", url: "" });
+  const {data:user} = useGetIdentity();
+	const {
+		refineCore: { onFinish, formLoading, queryResult },
+		register,
+		handleSubmit,
+		resetField,
+		formState: { errors },
+	} = useForm();
 
-const createProperty = () => {
-  return (
-    <div>createProperty</div>
-  )
-}
+  const handleImageChange = (file: File) => {
+    const reader = (readFile: File) => new Promise<string>((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
 
-export default createProperty
+    reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
+  };
+  
+  const onFinishHandler = async (data: FieldValues) => {
+    if(!propertyImage.name) return alert('Please select an image');
+    
+    await onFinish({ ...data, photo: propertyImage.url, email: user.email })
+  };
+
+	return ( 
+  <Form 
+    type="Create"
+    register = { register }
+    formLoading = { formLoading }
+    propertyImage = { propertyImage }
+    handleSubmit = { handleSubmit }
+    handleImageChange = { handleImageChange }
+    onFinish = { onFinish }
+    onFinishHandler = { onFinishHandler }
+  />);
+};
+
+export default CreateProperty;
