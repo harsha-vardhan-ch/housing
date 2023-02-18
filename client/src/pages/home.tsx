0f8da3b4adb47d495d2fa301b79dfa316f36a051
Harsha Vardhan <charagon@usc.edu>
2023-02-17 22:50:56 -0800

    Modified TotalRevenue Component

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,33 +1,61 @@
-import React from 'react'
-import {useList } from '@pankod/refine-core'
+import React from "react";
+import { useList } from "@pankod/refine-core";
 
-import { PieChart,
-  PropertyReferrals,
-  TotalRevenue,
-  PropertyCard,
-  TopAgent
-} from '../components/components_index'
+import {
+	PieChart,
+	PropertyReferrals,
+	TotalRevenue,
+	PropertyCard,
+	TopAgent,
+} from "../components/components_index";
 
-import { Typography, Box, Stack } from '@pankod/refine-mui'
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
 const home = () => {
-  return (
-    <Box>
-      
+	return (
+		<Box>
+			<Typography
+				fontSize={25}
+				fontWeight={700}
+				color="#11142D"
+			></Typography>
+			<Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+				<PieChart
+					title="Properties for sale"
+					value={684}
+					series={[75, 25]}
+					colors={["#475be8", "#e4e8ef"]}
+				/>
+				<PieChart
+					title="Properties for Rent"
+					value={498}
+					series={[60, 40]}
+					colors={["#475be8", "#e4e8ef"]}
+				/>
+				<PieChart
+					title="Total Customers"
+					value={5684}
+					series={[54, 46]}
+					colors={["#475be8", "#e4e8ef"]}
+				/>
+				<PieChart
+					title="Properties for Cities"
+					value={555}
+					series={[79, 21]}
+					colors={["#475be8", "#e4e8ef"]}
+				/>
+			</Box>
 
-      <Typography fontSize={25} fontWeight={700} color="#11142D">
+			<Stack
+				mt="25px"
+				width="100%"
+				direction={{ xs: "column", lg: "row" }}
+			>
+				<TotalRevenue />
+				<PropertyReferrals />
+			</Stack>
+		</Box>
+	);
+};
 
-      </Typography>
-      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
-        <PieChart title="Properties for sale" value={684} series={[75,25]} colors={['#475be8','#e4e8ef']}/>
-        <PieChart title="Properties for Rent" value={498} series={[60,40]} colors={['#475be8','#e4e8ef']}/>
-        <PieChart title="Total Customers" value={5684} series={[54,46]} colors={['#475be8','#e4e8ef']}/>
-        <PieChart title="Properties for Cities" value={555} series={[79,21]} colors={['#475be8','#e4e8ef']}/>
-
-       
-      </Box>
-      </Box>
-  )
-}
-
-export default home
+export default home;
