9331ea3f85aa944b235dfed43392a5d9fe90fe69
Harsha Vardhan <charagon@usc.edu>
2023-05-07 14:25:57 -0700

    deploy modifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -142,7 +142,7 @@ function App() {
 					dataProvider={dataProvider(
 						// "https://api.fake-rest.refine.dev"
             // "http://localhost:8080/api/v1"
-			"https://housease-render.onrender.com"
+			"https://housease-render.onrender.com/api/v1"
 					)}
 					notificationProvider={notificationProvider}
 					ReadyPage={ReadyPage}
